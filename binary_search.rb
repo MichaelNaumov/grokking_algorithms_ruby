70c67db6aa163dd77f22939380a001fe0afff89b
Michael Naumov <mihail.naumov23@gmail.com>
2019-10-08 22:49:22 +0300

    refactor

--- binary_search.rb
+++ binary_search.rb
@@ -1,21 +1,18 @@
-def binary_search list, item
-  low = 0
-  high = list.length - 1
+module BinarySearch
+  def self.binary_search list, item
+    low = 0
+    high = list.length - 1
 
-  while low <= high
-    mid = (high + low) / 2
-    guess = list[mid]
+    while low <= high
+      mid = (high + low) / 2
+      guess = list[mid]
 
-    p "Position of #{item} is #{mid}" if guess == item
-    if guess > item
-      high = mid - 1
-    else
-      low = mid + 1
+      return mid if guess == item
+      if guess > item
+        high = mid - 1
+      else
+        low = mid + 1
+      end
     end
   end
 end
-
-list = [1,3,4,5,6,7,8,9,10]
-i = 3
-
-binary_search list, i
--- selection_sort.rb
+++ selection_sort.rb
@@ -1,21 +1,19 @@
-def smallest arr
-  min = arr[0]
-  idx = 0
-  arr.each_with_index do |val, i|
-    min, idx = val, i if val < min
+module SelectionSort
+  def self.smallest arr
+    min = arr[0]
+    idx = 0
+    arr.each_with_index do |val, i|
+      min, idx = val, i if val < min
+    end
+    idx
   end
-  idx
-end
 
-def selection_sort arr
-  new_arr = []
-  (0..arr.length-1).each do |i|
-    min = smallest arr
-    new_arr.push(arr.delete_at(min))
+  def self.selection_sort arr
+    new_arr = []
+    (0..arr.length-1).each do |i|
+      min = smallest arr
+      new_arr.push(arr.delete_at(min))
+    end
+    new_arr
   end
-  p new_arr
 end
-
-arr = [7,6,5,4,2,2,1]
-
-selection_sort arr
