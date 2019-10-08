270566bc998a299544b7b8a329a17b6cceeff6ef
Michael Naumov <mihail.naumov23@gmail.com>
2019-10-08 22:48:08 +0300

    add tests

---
+++ binary_search_spec.rb
@@ -0,0 +1,10 @@
+require "rspec"
+require_relative "binary_search"
+
+describe BinarySearch do
+  it "returns proper position" do
+    list = [1,2,3,4,5,6,7,8,9]
+    guess = 8
+    expect(subject.binary_search(list, guess)).to eq(7 )
+  end
+end
---
+++ selection_sort_spec.rb
@@ -0,0 +1,11 @@
+require "rspec"
+require_relative "selection_sort"
+
+describe SelectionSort do
+  it "returns ordered array" do
+    array = [7,6,5,4,2,2,1]
+    ordered_array = array.sort
+
+    expect(subject.selection_sort(array)).to eq(ordered_array)
+  end
+end
